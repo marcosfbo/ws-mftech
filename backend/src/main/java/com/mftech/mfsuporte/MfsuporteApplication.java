8909266d9a64f384a716b4df8878086ee567e490
Marcos Fernando <marcosfbo@gmail.com>
2021-05-05 23:57:34 -0300

    Projeto criado

---
+++ backend/src/main/java/com/mftech/mfsuporte/MfsuporteApplication.java
@@ -0,0 +1,13 @@
+package com.mftech.mfsuporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfsuporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfsuporteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mftech/mfsuporte/MfsuporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mftech.mfsuporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfsuporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
