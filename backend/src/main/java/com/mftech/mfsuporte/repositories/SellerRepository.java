0d10d2367a674422e4209b6e592a232b114403dc
Marcos Fernando <marcosfbo@gmail.com>
2021-05-07 22:21:36 -0300

    Paginação

---
+++ backend/src/main/java/com/mftech/mfsuporte/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.mftech.mfsuporte.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mftech.mfsuporte.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
---
+++ backend/src/main/java/com/mftech/mfsuporte/service/SellerService.java
@@ -0,0 +1,23 @@
+package com.mftech.mfsuporte.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mftech.mfsuporte.dto.SellerDTO;
+import com.mftech.mfsuporte.entities.Seller;
+import com.mftech.mfsuporte.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+}
