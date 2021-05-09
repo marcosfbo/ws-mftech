2dfb89dd39997dedf48f04283a18e5b51a4e7457
Marcos Fernando <marcosfbo@gmail.com>
2021-05-09 09:34:38 -0300

    Agrupar por pesquisa

--- backend/src/main/java/com/mftech/mfsuporte/repositories/SaleRepository.java
+++ backend/src/main/java/com/mftech/mfsuporte/repositories/SaleRepository.java
@@ -1,9 +1,21 @@
 package com.mftech.mfsuporte.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.mftech.mfsuporte.dto.SaleSuccessDTO;
+import com.mftech.mfsuporte.dto.SaleSumDTO;
 import com.mftech.mfsuporte.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-
+	
+	@Query("SELECT new com.mftech.mfsuporte.dto.SaleSumDTO (obj.seller, SUM(obj.amount)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")//obj = apelido para cada objeto saller que trás do banco.
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.mftech.mfsuporte.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ " FROM Sale AS obj GROUP BY obj.seller")//obj = apelido para cada objeto saller que trás do banco.
+	List<SaleSuccessDTO> successGroupedBySeller(); // Sucesso agrupado por vendedor
 }
--- backend/src/main/java/com/mftech/mfsuporte/service/SaleService.java
+++ backend/src/main/java/com/mftech/mfsuporte/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.mftech.mfsuporte.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mftech.mfsuporte.dto.SaleDTO;
+import com.mftech.mfsuporte.dto.SaleSuccessDTO;
+import com.mftech.mfsuporte.dto.SaleSumDTO;
 import com.mftech.mfsuporte.entities.Sale;
 import com.mftech.mfsuporte.repositories.SaleRepository;
 import com.mftech.mfsuporte.repositories.SellerRepository;
@@ -25,5 +29,15 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return repository.successGroupedBySeller();
 	}
 }
