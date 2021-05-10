8720d773fd6c289a13280ac4d027c1fab83bb3f4
Marcos Fernando <marcosfbo@gmail.com>
2021-05-10 17:20:05 -0300

    Primeiro pedido

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,10 +1,35 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts'
+import { SaleSum } from 'types/sale';
+import { BASE_URL } from 'utils/request';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
+
 
 const DonutChart = () => { //função lambda (anônima)
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+
+    // Forma errada de declarar variavel chartData.
+    let chartData : ChartData = { labels: [], series: []}; //let = porque vai iniciar com um dado que muda.
+
+    // Forma errada
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    .then(response => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+
+        chartData = { labels: myLabels, series: mySeries};
+        console.log(chartData);
+    })
+
+
+    //const mockData = { //cosnt = constante, é um dado que não muda.
+    //    series: [477138, 499928, 444867, 220426, 473088],
+    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    //}
 
     const options = {
         legend: {
@@ -13,8 +38,8 @@ const DonutChart = () => { //função lambda (anônima)
     }
     return (
         <Chart
-            options={{ ...options, labels: mockData.labels }}
-            series={mockData.series}
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
         />
