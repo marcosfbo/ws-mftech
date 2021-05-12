ea7a07a7ffc858acbe6a1539b823615f66b36e56
Marcos Fernando <marcosfbo@gmail.com>
2021-05-12 08:29:28 -0300

    Comentando DataTable - variavel {item.seller.name}

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -45,7 +45,7 @@ const DataTable = () => {
                         {page.content?.map(item => (
                             <tr key={item.id}>
                                 <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
-                                <td>{item.seller.name}</td>
+                                <td>Nome do Técnico</td>
                                 <td>{item.visited}</td>
                                 <td>{item.deals}</td>
                                 <td>{item.amount.toFixed(2)}</td>
@@ -57,5 +57,5 @@ const DataTable = () => {
         </>
     );
 }
-
+//{item.seller.name}
 export default DataTable;
